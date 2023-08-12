4f8e9376a948ae57c85b7beebb55dbd9f37e36f7
Richa <richaverma13031975@gmail.com>
2023-08-12 12:43:03 +0530

    Cart Page added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import Home from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
+import CartPage from "./pages/CartPage";
 
 import {
   createBrowserRouter,
@@ -26,6 +27,11 @@ const router = createBrowserRouter([
     path: "/signup",
     element: <SignupPage></SignupPage>,
   },
+  {
+    path: "/cart",
+    element: <CartPage></CartPage>,
+  },
+
 ]);
 
 function App() {
